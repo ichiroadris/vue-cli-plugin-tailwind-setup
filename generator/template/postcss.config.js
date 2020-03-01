a5f45377fb708bb55ce73d95cf225041d1361a28
ichiroadris <ichiroadris@gmail.com>
2020-03-01 18:43:05 +0800

    new config

--- generator/template/postcss.config.js
+++ generator/template/postcss.config.js
@@ -1,13 +1,9 @@
-const postcss = require('postcss');
-
-let config = {
-  content: ['./public/**/*.html', './src/**/*.vue'],
-  defaultExtractor: content =>
-    content.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [],
-};
-
-module.exports = postcss.plugin('tailwind-purgecss', function (opts) {
-  if (process.env.NODE_ENV !== 'production') return () => { };
-  const purgecss = require('@fullhuman/postcss-purgecss');
-  return purgecss(Object.assign(config, opts));
-});
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+module.exports = {
+  plugins: {
+    tailwindcss,
+    autoprefixer,
+    'vue-cli-plugin-tailwind-setup/purgecss': {}
+  }
+}
